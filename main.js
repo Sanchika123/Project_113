8e06ef4c13f551e7c40efb96fa80564ebc10ea59
Sanchika123 <79789552+Sanchika123@users.noreply.github.com>
2021-08-11 22:58:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+function preload() {
+    img=loadImage('https://i.postimg.cc/VNhD7psd/Astronaut-planet-stars-beautiful-space-2880x1800.jpg');
+    }
+    
+    function setup() {
+    createCanvas(700,550);
+    }
+    
+    function draw() {
+    image(img,285,220,200,120);
+    
+    fill(244,194,194);
+    stroke(0, 0, 0);
+    rect(600,150,20,250);
+      
+    fill(244,194,194);
+    stroke(0, 0, 0);
+    rect(150,150,20,250);
+      
+    fill(244,194,194);
+    stroke(0, 0, 0);
+    rect(150,150,450,20);
+      
+    fill(244,194,194);
+    stroke(0, 0, 0);
+    rect(150,400,450,20);
+      
+    fill(134,226,244);
+    stroke(0, 0, 0);
+    ellipse(160, 160, 55, 55);
+      
+    fill(134,226,244);
+    stroke(0, 0, 0);
+    ellipse(610, 160, 55, 55);
+    
+    fill(134,226,244);
+    stroke(0, 0, 0);
+    ellipse(610, 410, 55, 55);
+      
+    fill(134,226,244);
+    stroke(0, 0, 0);
+    ellipse(160, 410, 55, 55);
+    }
+
+
+
+
+
+     
+    
+
+   
